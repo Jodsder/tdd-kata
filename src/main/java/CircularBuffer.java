46aeb1b93b62a83a40b7c36276fad6cee1dd08e2
Joel Suter <joel.suter@ost.ch>
2022-04-26 14:21:28 +0200

    Updated CircularBuffer

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,18 +1,32 @@
 import java.nio.BufferUnderflowException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CircularBuffer<T> {
 
    private int size;
+   private List<T> buffer;
 
    public CircularBuffer(int size) {
       this.size = size;
+      buffer = new ArrayList<>();
    }
 
    public int getSize() {
       return this.size;
    }
 
+   public void add(T element){
+      if(buffer.size()>=size){
+         remove();
+      }
+      buffer.add(element);
+   }
+
    public T remove() throws BufferUnderflowException{
+      if(buffer.size()>0) {
+         return buffer.remove(0);
+      }
       throw new BufferUnderflowException();
    }
 
