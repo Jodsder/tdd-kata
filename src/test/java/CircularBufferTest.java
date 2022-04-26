dad059ca2bdd80e8b0003ee18c9a6b17d40db812
Joel Suter <joel.suter@ost.ch>
2022-04-26 13:15:42 +0200

    added another test

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -8,4 +8,10 @@ public class CircularBufferTest {
       CircularBuffer buffer = new CircularBuffer(0);
       assertEquals(0, buffer.getSize());
    }
+
+   @Test
+   public void circularBufferInitialzedWith5() {
+      CircularBuffer buffer = new CircularBuffer(5);
+      assertEquals(5, buffer.getSize());
+   }
 }
