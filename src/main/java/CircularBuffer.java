84d2893729b2fe7c25762dc654787d01a5c2757d
Joel Suter <joel.suter@ost.ch>
2022-04-26 13:14:50 +0200

    Cycle 1(Pass): Make size test pass

--- src/main/java/CircularBuffer.java
+++ src/main/java/CircularBuffer.java
@@ -1,3 +1,13 @@
 public class CircularBuffer {
 
+   private int size;
+
+   public CircularBuffer(int size) {
+      this.size = size;
+   }
+
+   public int getSize() {
+      return this.size;
+   }
+
 }
