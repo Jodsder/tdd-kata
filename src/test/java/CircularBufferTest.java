816c04855d2ef31af45160d864a1378f2c8afaee
Joel Suter <joel.suter@ost.ch>
2022-04-26 13:12:26 +0200

    Cycle 1(Test): Testing if circularbuffer has a size

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -1,3 +1,11 @@
-public class CircularBufferTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CircularBufferTest {
+   @Test
+   public void circularBufferInitialzedEmpty() {
+      CircularBuffer buffer = new CircularBuffer(0);
+      assertEquals(0, buffer.getSize());
+   }
 }
